689d7031f1146816c83c97ed4e012d2d8d8c90bc
Dejene <129173441+Dejene10@users.noreply.github.com>
2023-05-17 11:53:59 -0700

    Add files via upload

---
+++ QUIZE APP1/questions.js
@@ -0,0 +1,70 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Who is making the web standards?",
+  answer: "World Wide Web Consortium",
+  options: [
+    "Google",
+    "Mozilla",
+    "Microsoft",
+    "World Wide Web Consortium"
+  ]
+},
+  {
+  numb: 2,
+  question: "Block elements are normally displayed without starting a new line.?",
+  answer: "True",
+  options: [
+    "True",
+    "False",
+    "Neither true nor false",
+    "none"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+  answer: "Alt",
+  options: [
+    "Title",
+    "Src",
+    "Alt",
+    "Longdesc"
+  ]
+},
+  {
+  numb: 4,
+  question: "Graphics defined by SVG is in which format?",
+  answer: "XML",
+  options: [
+    "CSS",
+    "HTML",
+    "XML",
+    "ALL"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which input type defines a slider control?",
+  answer: "range",
+  options: [
+    "search",
+    "slider",
+    "controls",
+    "range"
+  ]
+},
+{
+  numb: 6,
+  question: "Which character is used to indicate an end tag?  ",
+  answer: "/",
+  options: [
+    "<",
+    "/",
+    "*",
+    "^"
+  ]
+},
+
+];
